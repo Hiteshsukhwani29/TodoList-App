aded8dbfb4ea4bb3713b01a4608229ed44a05988
Hiteshsukhwani29 <hiteshsukhwani29@gmail.com>
2022-07-26 18:02:35 +0530

    Add delete icon

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={tw`mt-4 mx-5`}>
-        <Text style={tw`text-blue-500 text-2xl font-bold`}>Thoughts</Text>
+        <Text style={tw`text-blue-500 text-2xl font-bold mt-3`}>Thoughts</Text>
         <View style={{ paddingVertical: 10 }}>
           <Thought />
           <Thought />
@@ -21,6 +21,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f9f9f9",
   },
 });
--- components/Thoughts/index.js
+++ components/Thoughts/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import tw from 'tailwind-react-native-classnames';
+import Icon from "../../assets/delete.png";
 
 const Index = () => {
   return (
@@ -11,12 +12,20 @@ const Index = () => {
           borderColor: "#eeeeee",
           borderRadius: 8,
           borderWidth: 1,
-          padding: 20
+          padding: 20,
+          display: "flex",
+          flexDirection:"row",
+          alignItems:"center"
         }}
       >
+        <View style={{flexGrow:1}}>
         <Text style={tw`text-blue-400 text-xl font-bold`}>Title</Text>
         <Text style={tw`text-xs font-bold`}>Date</Text>
         <Text style={tw`text-sm mt-2`}>Description</Text>
+        </View>
+        <View style={{}}>
+          <Image style={tw`w-6 h-6`} source={Icon}/>
+        </View>
       </View>
     </View>
   );
